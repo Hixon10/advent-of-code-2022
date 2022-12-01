983d46264a5433924ff56c058d8c7a72e8bb87df
Denis <hixon10@yandex.ru>
2022-12-01 20:39:37 +0000

    init

---
+++ Day1.cs
@@ -0,0 +1,9 @@
+﻿namespace advent_of_code_2022;
+
+public class Day1
+{
+    public static void Solve(string[] args)
+    {
+        System.Console.WriteLine("Solve1!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace advent_of_code_2022;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Day1.Solve(args);
+        // Day2.Solve(args);
+    }
+}
